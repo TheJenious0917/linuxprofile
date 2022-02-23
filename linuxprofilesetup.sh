74b389f267f7cbed698a947353dd66d0ea8460ea
Chris <69024420+TheJenious0917@users.noreply.github.com>
2022-02-23 01:13:11 -0600

    update linux profile setup

--- linuxprofilesetup.sh
+++ linuxprofilesetup.sh
@@ -26,5 +26,30 @@ cd ~
 # pulls custom zshrc configuration from github so it's configured already
 curl -LJO https://raw.githubusercontent.com/TheJenious0917/linuxprofile/main/.zshrc
 
-# starts zsh
-zsh
+# installs vscode
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+sudo apt install apt-transport-https -y
+sudo apt update
+sudo apt install code -y
+
+# installs MS Edge beta
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/edge stable main" > /etc/apt/sources.list.d/microsoft-edge-beta.list'
+sudo rm microsoft.gpg
+sudo apt update
+sudo apt install microsoft-edge-beta -y
+
+# clean up fonts
+sudo apt remove "fonts-kacst*" "fonts-khmeros*" fonts-lklug-sinhala fonts-guru-extra "fonts-nanum*" fonts-noto-cjk "fonts-takao*" \
+fonts-tibetan-machine fonts-lao fonts-sil-padauk fonts-sil-abyssinica "fonts-tlwg-*" "fonts-lohit-*" fonts-beng-extra fonts-gargi \
+fonts-gubbi fonts-gujr-extra fonts-kalapi "fonts-samyak*" fonts-navilu fonts-nakula fonts-orya-extra fonts-pagul fonts-sarai \
+"fonts-telu*" "fonts-wqy*" "fonts-smc*" fonts-deva-extra fonts-sahadeva -y
+sudo dpkg-reconfigure fontconfig
+
+# disable local linux firewall logging
+sudo ufw logging off
+
